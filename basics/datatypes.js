cc38f6db0373073b1f70d75ab2690b9271307a13
Keyur Solanki <59692585+solankikeyur@users.noreply.github.com>
2023-10-06 07:05:11 +0000

    Variables and datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+/* 
+
+When using "use strict" this will treat all JS code as newer version
+
+ECMA script defines javascript standards
+
+number
+bigint
+string
+boolean
+null => standalone value
+undefined => variable is declared but no value is assigned
+symbol => unique & cannot be changed
+object
+
+typeof undefined is undefines
+typeof null is object
+
+*/
+
---
+++ basics/variables.js
@@ -0,0 +1,21 @@
+/* 
+
+const => Cannot change value
+
+There two ways to declare variables let & var
+
+Donot use var only use let bcz of issue with block scope or functional scope
+
+Declaring empty variable like qualification will result in undefined
+
+Cannot redeclare when declared with let
+
+*/
+
+const speedLimit = 80;
+let name = "Keyur Solanki";
+var profession = "Software Developer";
+let qualification;
+
+console.log([speedLimit, name, profession, qualification, ok]);
+
